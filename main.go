5ac7540ad547fe895b8d73a8db18669b600b2550
Hugo Peixoto <hugo.peixoto@gmail.com>
2018-02-25 23:17:49 +0000

    go fmt code

--- main.go
+++ main.go
@@ -1,243 +1,247 @@
 package main
 
 import (
-  "crypto"
-  "crypto/elliptic"
-  "crypto/ecdsa"
-  "crypto/rand"
-  "crypto/tls"
-  "crypto/x509"
-  "encoding/hex"
-  "encoding/json"
-  "errors"
-  "log"
-  "io/ioutil"
-  "github.com/xenolf/lego/acme"
-  "net/http"
-  "net/url"
-  "net/http/httputil"
-  "strings"
-  "sync"
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"github.com/xenolf/lego/acme"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+	"sync"
 )
 
 type JSONSerializablePrivateKey struct {
-  Key *ecdsa.PrivateKey
+	Key *ecdsa.PrivateKey
 }
 
 type User struct {
-  Email string
-  Registration *acme.RegistrationResource
-  Key JSONSerializablePrivateKey
+	Email        string
+	Registration *acme.RegistrationResource
+	Key          JSONSerializablePrivateKey
 }
 
 func (u User) GetEmail() string { return u.Email }
 
 func (u User) GetRegistration() *acme.RegistrationResource { return u.Registration }
-func (u User) GetPrivateKey() crypto.PrivateKey { return u.Key.Key }
+func (u User) GetPrivateKey() crypto.PrivateKey            { return u.Key.Key }
 
 func (jspk JSONSerializablePrivateKey) MarshalJSON() ([]byte, error) {
-  bytes, err := x509.MarshalECPrivateKey(jspk.Key)
-  if err != nil {
-    return []byte{}, err
-  }
+	bytes, err := x509.MarshalECPrivateKey(jspk.Key)
+	if err != nil {
+		return []byte{}, err
+	}
 
-  return json.Marshal(string(hex.EncodeToString(bytes)))
+	return json.Marshal(string(hex.EncodeToString(bytes)))
 }
 
 func (jspk *JSONSerializablePrivateKey) UnmarshalJSON(payload []byte) error {
-  hexstring := ""
-   err := json.Unmarshal(payload, &hexstring)
-  if err != nil {
-    return err
-  }
+	hexstring := ""
+	err := json.Unmarshal(payload, &hexstring)
+	if err != nil {
+		return err
+	}
 
-  bytes, err := hex.DecodeString(hexstring)
-  if err != nil {
-    return err
-  }
+	bytes, err := hex.DecodeString(hexstring)
+	if err != nil {
+		return err
+	}
 
-  key, err := x509.ParseECPrivateKey(bytes)
+	key, err := x509.ParseECPrivateKey(bytes)
 
-  jspk.Key = key
+	jspk.Key = key
 
-  return err
+	return err
 }
 
-
 func ReadJSON(filename string, target interface{}) error {
-  data, err := ioutil.ReadFile(filename)
+	data, err := ioutil.ReadFile(filename)
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  return json.Unmarshal(data, target)
+	return json.Unmarshal(data, target)
 }
 
 func NewClient(endpoint string, filename string, email string) (*acme.Client, error) {
-  user := User{}
-  err := ReadJSON(filename, &user)
-  if err != nil {
-    log.Println(err)
-    pkey, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
-    if err != nil {
-      return nil, err
-    }
+	user := User{}
+	err := ReadJSON(filename, &user)
+	if err != nil {
+		log.Println(err)
+		pkey, err := ecdsa.GenerateKey(elliptic.P384(), rand.Reader)
+		if err != nil {
+			return nil, err
+		}
 
-    user = User{Email: email, Key: JSONSerializablePrivateKey{Key: pkey}}
-  }
+		user = User{Email: email, Key: JSONSerializablePrivateKey{Key: pkey}}
+	}
 
-  client, err := acme.NewClient(endpoint, &user, acme.EC384)
-  if err != nil {
-    return nil, err
-  }
+	client, err := acme.NewClient(endpoint, &user, acme.EC384)
+	if err != nil {
+		return nil, err
+	}
 
-  if user.Registration == nil {
-    reg, err := client.Register()
-    if err != nil {
-      return nil, err
-    }
+	if user.Registration == nil {
+		reg, err := client.Register()
+		if err != nil {
+			return nil, err
+		}
 
-    user.Registration = reg
+		user.Registration = reg
 
-    payload, err := json.Marshal(user)
-    if err != nil {
-      return nil, err
-    }
+		payload, err := json.Marshal(user)
+		if err != nil {
+			return nil, err
+		}
 
-    err = ioutil.WriteFile(filename, payload, 0600)
-    if err != nil {
-      return nil, err
-    }
-  }
+		err = ioutil.WriteFile(filename, payload, 0600)
+		if err != nil {
+			return nil, err
+		}
+	}
 
-  log.Println("pkey")
-  log.Println(user.Key.Key)
+	log.Println("pkey")
+	log.Println(user.Key.Key)
 
-  client.AgreeToTOS()
+	client.AgreeToTOS()
 
-  return client, err
+	return client, err
 }
 
 type Proxy struct {
-  Domains []string `json:"domains"`
-  Target string `json:"target"`
+	Domains []string `json:"domains"`
+	Target  string   `json:"target"`
 }
 
 type Settings struct {
-  Proxies []Proxy `json:"proxies"`
+	Proxies []Proxy `json:"proxies"`
 }
 
 func (s *Settings) GetTarget(serverName string) (*url.URL, error) {
-  for _, proxy := range s.Proxies {
-    for _, domain := range proxy.Domains {
-      if domain == serverName {
-        return url.Parse(proxy.Target)
-      }
-    }
-  }
+	for _, proxy := range s.Proxies {
+		for _, domain := range proxy.Domains {
+			if domain == serverName {
+				return url.Parse(proxy.Target)
+			}
+		}
+	}
 
-  return nil, errors.New("unknown server name")
+	return nil, errors.New("unknown server name")
 }
 
 type CertificateStore struct {
-  lock sync.RWMutex
-  certificates map[string]*tls.Certificate
+	lock         sync.RWMutex
+	certificates map[string]*tls.Certificate
 }
 
 func (cs *CertificateStore) GetCertificate(serverName string) (*tls.Certificate, error) {
-  cs.lock.RLock()
-  defer cs.lock.RUnlock()
+	cs.lock.RLock()
+	defer cs.lock.RUnlock()
 
-  if cs.certificates[serverName] != nil {
-    return cs.certificates[serverName], nil
-  }
+	if cs.certificates[serverName] != nil {
+		return cs.certificates[serverName], nil
+	}
 
-  return nil, errors.New("unknown server name")
+	return nil, errors.New("unknown server name")
 }
 
 func (cs *CertificateStore) SetCertificate(serverNames []string, cert *tls.Certificate) {
-  cs.lock.Lock()
-  defer cs.lock.Unlock()
+	cs.lock.Lock()
+	defer cs.lock.Unlock()
 
-  for _, serverName := range serverNames {
-    cs.certificates[serverName] = cert
-  }
+	for _, serverName := range serverNames {
+		cs.certificates[serverName] = cert
+	}
 }
 
 func main() {
-  client, err := NewClient("https://acme-staging.api.letsencrypt.org/directory", "tmp/user.json", "hugo.peixoto@gmail.com")
-  log.Println(err)
+	client, err := NewClient(
+		"https://acme-staging.api.letsencrypt.org/directory",
+		"tmp/user.json",
+		"hugo.peixoto@gmail.com",
+	)
 
-  client.SetHTTPAddress(":8080")
+	log.Println(err)
 
-  settings := Settings{}
+	client.SetHTTPAddress(":8080")
 
-  certstore := CertificateStore{}
+	settings := Settings{}
 
-  err = ReadJSON("tmp/proxy.json", &settings)
-  log.Println(err)
+	certstore := CertificateStore{}
 
-  for _, proxy := range settings.Proxies {
-    certRes, failures := client.ObtainCertificate(proxy.Domains, true, nil, false)
+	err = ReadJSON("tmp/proxy.json", &settings)
+	log.Println(err)
 
-    if len(failures) > 0 {
-      log.Fatalln(failures)
-    }
+	for _, proxy := range settings.Proxies {
+		certRes, failures := client.ObtainCertificate(proxy.Domains, true, nil, false)
 
-    cert, err := tls.X509KeyPair(certRes.Certificate, certRes.PrivateKey)
-    if err != nil {
-      panic(err)
-    }
+		if len(failures) > 0 {
+			log.Fatalln(failures)
+		}
 
-    certstore.SetCertificate(proxy.Domains, &cert)
-  }
+		cert, err := tls.X509KeyPair(certRes.Certificate, certRes.PrivateKey)
+		if err != nil {
+			panic(err)
+		}
 
-  server := http.Server{
-    Addr: ":https",
-    TLSConfig: &tls.Config{
-      GetCertificate: func(chi *tls.ClientHelloInfo) (*tls.Certificate, error) {
-        return certstore.GetCertificate(chi.ServerName)
-      },
-    },
-    Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-      target, err := settings.GetTarget(r.Host)
+		certstore.SetCertificate(proxy.Domains, &cert)
+	}
 
-      if err != nil {
-        w.WriteHeader(404)
-        w.Write([]byte("unknown domain "))
-        w.Write([]byte(r.Host))
-        w.Write([]byte("\n"))
-      } else {
-        httputil.NewSingleHostReverseProxy(target).ServeHTTP(w, r)
-      }
-    }),
-  }
+	server := http.Server{
+		Addr: ":https",
+		TLSConfig: &tls.Config{
+			GetCertificate: func(chi *tls.ClientHelloInfo) (*tls.Certificate, error) {
+				return certstore.GetCertificate(chi.ServerName)
+			},
+		},
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			target, err := settings.GetTarget(r.Host)
 
-  serverhttp := http.Server{
-    Addr: ":http",
-    Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-      _, err := settings.GetTarget(r.Host)
+			if err != nil {
+				w.WriteHeader(404)
+				w.Write([]byte("unknown domain "))
+				w.Write([]byte(r.Host))
+				w.Write([]byte("\n"))
+			} else {
+				httputil.NewSingleHostReverseProxy(target).ServeHTTP(w, r)
+			}
+		}),
+	}
 
-      if err != nil {
-        w.WriteHeader(404)
-        w.Write([]byte("unknown domain "))
-        w.Write([]byte(r.Host))
-        w.Write([]byte("\n"))
-      } else {
-        if strings.HasPrefix(r.RequestURI, "/.well-known/acme-challenge/") {
-          p, _ := url.Parse("http://localhost:8080")
+	serverhttp := http.Server{
+		Addr: ":http",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, err := settings.GetTarget(r.Host)
 
-          httputil.NewSingleHostReverseProxy(p).ServeHTTP(w, r)
-        } else {
-          http.Redirect(w, r, "https://" + r.Host + r.RequestURI, http.StatusMovedPermanently)
-        }
-      }
-    }),
-  }
+			if err != nil {
+				w.WriteHeader(404)
+				w.Write([]byte("unknown domain "))
+				w.Write([]byte(r.Host))
+				w.Write([]byte("\n"))
+			} else {
+				if strings.HasPrefix(r.RequestURI, "/.well-known/acme-challenge/") {
+					p, _ := url.Parse("http://localhost:8080")
 
-  go func() { panic(server.ListenAndServeTLS("", "")) }()
-  go func() { panic(serverhttp.ListenAndServe()) }()
+					httputil.NewSingleHostReverseProxy(p).ServeHTTP(w, r)
+				} else {
+					http.Redirect(w, r, "https://"+r.Host+r.RequestURI, http.StatusMovedPermanently)
+				}
+			}
+		}),
+	}
 
-  select {}
+	go func() { panic(server.ListenAndServeTLS("", "")) }()
+	go func() { panic(serverhttp.ListenAndServe()) }()
+
+	select {}
 }
